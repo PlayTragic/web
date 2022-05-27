11409305a5ea9dec429cf446438b0751070ca5a6
PlayTragic <63451317+PlayTragic@users.noreply.github.com>
2022-05-27 12:30:38 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+    $("#header").load("header.html");
+});
+
+function openAbt() {
+    $('#about').load("about.html");
+}
+
+function closeAbt() {
+    $('#about').empty("about.html");
+}
+$(function() {
+    $("#footer").load("footer.html");
+});
